48d546aaea008369422059da6f8cec8bd8fb7410
favoritas37 <ftylitak@gmail.com>
2015-07-16 21:09:44 +0300

    Added the example project BarcodeEncoder which will be used as the base for the barcode encoding functions (and testing)

---
+++ examples/BarcodeEncoder/main.cpp
@@ -0,0 +1,17 @@
+#include <QtGui/QGuiApplication>
+#include "qtquick2applicationviewer.h"
+
+#include <QZXing.h>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QZXing::registerQMLTypes();
+    QtQuick2ApplicationViewer viewer;
+    viewer.setMainQmlFile(QStringLiteral("qml/BarcodeEncoder/main.qml"));
+
+    viewer.showExpanded();
+
+    return app.exec();
+}
