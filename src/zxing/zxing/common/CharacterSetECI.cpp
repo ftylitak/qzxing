83243790f85fdfcd26ec501c524ac0cc310f2ba1
favoritas37 <ftylitak@gmail.com>
2019-03-25 12:30:23 +0200

    simplified CharacterSetECI initialization

--- src/zxing/zxing/common/CharacterSetECI.cpp
+++ src/zxing/zxing/common/CharacterSetECI.cpp
@@ -32,68 +32,70 @@ std::vector<CharacterSetECI*> CharacterSetECI::ECItables;
 
 bool CharacterSetECI::inited = CharacterSetECI::init_tables();
 
-#define ADD_CHARACTER_SET(VALUES, STRINGS) \
-  { static int values[] = {VALUES, -1}; \
-    static char const* strings[] = {STRINGS, 0}; \
-    addCharacterSet(values, strings); }
+#define COMMA ,
 
-#define XC ,
+#define ADD_CHARACTER_SET(VALUES, STRINGS) \
+  { int values[] = {VALUES}; \
+    const char *strings[] = {STRINGS}; \
+    addCharacterSet(std::vector<int>(values, values + sizeof(values) / sizeof(values[0])), \
+                    std::vector<const char*>(strings, strings + sizeof(strings) / sizeof(strings[0]))); }
 
 bool CharacterSetECI::init_tables() {
-  ADD_CHARACTER_SET(0 XC 2, "Cp437");
-  ADD_CHARACTER_SET(1 XC 3, "ISO8859_1" XC "ISO-8859-1");
-  ADD_CHARACTER_SET(4, "ISO8859_2" XC "ISO-8859-2");
-  ADD_CHARACTER_SET(5, "ISO8859_3" XC "ISO-8859-3");
-  ADD_CHARACTER_SET(6, "ISO8859_4" XC "ISO-8859-4");
-  ADD_CHARACTER_SET(7, "ISO8859_5" XC "ISO-8859-5");
-  ADD_CHARACTER_SET(8, "ISO8859_6" XC "ISO-8859-6");
-  ADD_CHARACTER_SET(9, "ISO8859_7" XC "ISO-8859-7");
-  ADD_CHARACTER_SET(10, "ISO8859_8" XC "ISO-8859-8");
-  ADD_CHARACTER_SET(11, "ISO8859_9" XC "ISO-8859-9");
-  ADD_CHARACTER_SET(12, "ISO8859_10" XC "ISO-8859-10");
-  ADD_CHARACTER_SET(13, "ISO8859_11" XC "ISO-8859-11");
-  ADD_CHARACTER_SET(15, "ISO8859_13" XC "ISO-8859-13");
-  ADD_CHARACTER_SET(16, "ISO8859_14" XC "ISO-8859-14");
-  ADD_CHARACTER_SET(17, "ISO8859_15" XC "ISO-8859-15");
-  ADD_CHARACTER_SET(18, "ISO8859_16" XC "ISO-8859-16");
-  ADD_CHARACTER_SET(20, "SJIS" XC "Shift_JIS");
-  ADD_CHARACTER_SET(21, "Cp1250" XC "windows-1250");
-  ADD_CHARACTER_SET(22, "Cp1251" XC "windows-1251");
-  ADD_CHARACTER_SET(23, "Cp1252" XC "windows-1252");
-  ADD_CHARACTER_SET(24, "Cp1256" XC "windows-1256");
-  ADD_CHARACTER_SET(25, "UnicodeBigUnmarked" XC "UTF-16BE" XC "UnicodeBig");
-  ADD_CHARACTER_SET(26, "UTF8" XC "UTF-8");
-  ADD_CHARACTER_SET(27 XC 170, "ASCII" XC "US-ASCII");
+  ADD_CHARACTER_SET(1 COMMA 3, "ISO8859_1" COMMA "ISO-8859-1")
+  ADD_CHARACTER_SET(0 COMMA 2, "Cp437");
+  ADD_CHARACTER_SET(4, "ISO8859_2" COMMA "ISO-8859-2");
+  ADD_CHARACTER_SET(5, "ISO8859_3" COMMA "ISO-8859-3");
+  ADD_CHARACTER_SET(6, "ISO8859_4" COMMA "ISO-8859-4");
+  ADD_CHARACTER_SET(7, "ISO8859_5" COMMA "ISO-8859-5");
+  ADD_CHARACTER_SET(8, "ISO8859_6" COMMA "ISO-8859-6");
+  ADD_CHARACTER_SET(9, "ISO8859_7" COMMA "ISO-8859-7");
+  ADD_CHARACTER_SET(10, "ISO8859_8" COMMA "ISO-8859-8");
+  ADD_CHARACTER_SET(11, "ISO8859_9" COMMA "ISO-8859-9");
+  ADD_CHARACTER_SET(12, "ISO8859_10" COMMA "ISO-8859-10");
+  ADD_CHARACTER_SET(13, "ISO8859_11" COMMA "ISO-8859-11");
+  ADD_CHARACTER_SET(15, "ISO8859_13" COMMA "ISO-8859-13");
+  ADD_CHARACTER_SET(16, "ISO8859_14" COMMA "ISO-8859-14");
+  ADD_CHARACTER_SET(17, "ISO8859_15" COMMA "ISO-8859-15");
+  ADD_CHARACTER_SET(18, "ISO8859_16" COMMA "ISO-8859-16");
+  ADD_CHARACTER_SET(20, "SJIS" COMMA "Shift_JIS");
+  ADD_CHARACTER_SET(21, "Cp1250" COMMA "windows-1250");
+  ADD_CHARACTER_SET(22, "Cp1251" COMMA "windows-1251");
+  ADD_CHARACTER_SET(23, "Cp1252" COMMA "windows-1252");
+  ADD_CHARACTER_SET(24, "Cp1256" COMMA "windows-1256");
+  ADD_CHARACTER_SET(25, "UnicodeBigUnmarked" COMMA "UTF-16BE" COMMA "UnicodeBig");
+  ADD_CHARACTER_SET(26, "UTF8" COMMA "UTF-8");
+  ADD_CHARACTER_SET(27 COMMA 170, "ASCII" COMMA "US-ASCII");
   ADD_CHARACTER_SET(28, "Big5");
-  ADD_CHARACTER_SET(29, "GB18030" XC "GB2312" XC "EUC_CN" XC "GBK");
-  ADD_CHARACTER_SET(30, "EUC_KR" XC "EUC-KR");
+  ADD_CHARACTER_SET(29, "GB18030" COMMA "GB2312" COMMA "EUC_CN" COMMA "GBK");
+  ADD_CHARACTER_SET(30, "EUC_KR" COMMA "EUC-KR");
 
   std::atexit(removeAllCharacterSets);
 
   return true;
 }
 
-#undef XC
-
-CharacterSetECI::CharacterSetECI(int const* values,
-                                 char const* const* names) 
-  : Counted(), values_(values), names_(names) {}
+CharacterSetECI::CharacterSetECI(const std::vector<int> values, const std::vector<const char*> names)
+  : Counted(), values_(values)
+{
+    for(size_t i=0; i<names.size(); i++)
+        names_.push_back(std::string(names[i]));
+}
 
 char const* CharacterSetECI::name() const {
-  return names_[0];
+  return names_[0].c_str();
 }
 
 int CharacterSetECI::getValue() const {
   return values_[0];
 }
 
-void CharacterSetECI::addCharacterSet(int const* values, char const* const* names) {
+void CharacterSetECI::addCharacterSet(const std::vector<int> values, const std::vector<const char*> names) {
   CharacterSetECI* charSet = new CharacterSetECI(values, names);
-  for(int const* values_tmp = values; *values_tmp != -1; values_tmp++) {
-    VALUE_TO_ECI[*values_tmp] = charSet;
+  for(size_t i=0; i<values.size(); i++) {
+    VALUE_TO_ECI[values[i]] = charSet;
   }
-  for(char const* const* names_tmp = names; *names_tmp; names_tmp++) {
-    NAME_TO_ECI[string(*names_tmp)] = charSet;
+  for(size_t i=0; i<names.size(); i++) {
+    NAME_TO_ECI[std::string(names[i])] = charSet;
   }
 
   ECItables.push_back(charSet);
